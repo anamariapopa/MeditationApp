8f3cf80867863c95a557df0116ef1e5906d3635a
anamariapopa <anamaria.2005p@gmail.com>
2026-02-28 23:49:50 +0200

    Initial commit

---
+++ src/main/java/com/meditate/MeditationApp/MeditationAppApplication.java
@@ -0,0 +1,13 @@
+package com.meditate.MeditationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeditationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meditate/MeditationApp/MeditationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meditate.MeditationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
